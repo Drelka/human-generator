45082cfdc303e120dc1efcbced1d4ee0f8cbcfa5
Grzegorz <47503020+Drelka@users.noreply.github.com>
2023-08-13 21:06:38 +0100

    first steps

---
+++ human-generator/generator.js
@@ -0,0 +1,14 @@
+genderX = ['male', 'female'];
+ageX = [Math.round(Math.random()*100)];
+heightX = 
+positiveTraitX = 
+negativeTraitX = 
+
+human = {
+    gender: 'male / female',
+    age: '0-100',
+    height: '140-200',
+    positiveTrait: 'Active, Admirable, Adventurous, Agreeable, Amiable, Amusing,, Appreciative, Athletic, Authentic, Benevolent, Brave, Bright, Brilliant, Calm, Capable, Caring, Charming, Cheerful, Clean, Clear-headed, Clever, Compassionate, Confident, Considerate, Cooperative, Courageous, Courteous, Creative, Curious, Dedicated, Easygoing, Educated, Enthusiastic, Ethical, Exciting, Extraordinary, Fair, Firm, Focused, Forgiving, Friendly, Generous, Gentle, Good-natured, Grateful, Happy, Hardworking, Helpful, Heroic, Honest, Hopeful, Humble, Innocent, Intelligent, Inventive, Joyful, Kind, Lively, Loving, Loyal, Neat, Nice, Optimistic, Organized, Passionate, Patient, Peaceful, Playful, Polite, Principled, Reliable, Respectful, Responsible, Self-disciplined, Selfless, Sincere, Skillful, Strong, Sweet, Thoughtful, Trustworthy, Understanding, Unselfish, Wise',
+    negativeTrait: 'Aggressive, Angry, Anxious, Argumentative, Arrogant, Bored, Bossy, Brutal, Careless, Charmless, Clumsy, Conceited, Cowardly, Critical, Cruel, Dangerous, Deceitful, Destructive, Devious, Difficult, Discouraging, Discourteous, Dishonest, Disloyal, Disobedient, Disorganized, Disrespectful, Disruptive, Envious, Fearful, Foolish, Forgetful, Frightening, Gloomy, Greedy, Grim, Hateful, Haughty, Hostile, Ignorant, Impatient, Impractical, Inconsiderate, Insincere, Insulting, Intolerant, Irresponsible, Irritable, Jealous, Lazy, Liar, Mean, Meddlesome, Messy, Miserable, Monstrous, Moody, Negative, Neglectful, Obnoxious, Petty, Possessive, Power-hungry, Prejudiced, Resentful, Rude, Scornful, Selfish, Shallow, Sloppy, Sneaky, Snobbish, Thoughtless, Unappreciative, Uncaring, Uncooperative, Unforgiving, Unfriendly, Ungrateful, Unhealthy, Unreliable, Violent, Weak, Wicked',
+};
+
